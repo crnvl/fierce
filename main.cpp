ccc8c19a11fcfaec099a63dcdec1c81cd607e882
angelsflyinhell <azami@namespace.media>
2023-01-22 14:41:44 +0100

    Add comments because yays and fun

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int main() {
 
+	// Prep stuff (not important) (that's a lie)
 	glfwInit();
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -20,12 +21,21 @@ int main() {
 
 	gladLoadGL();
 
+	// Interesting stuff
 	glViewport(0, 0, 800, 800);
 
+	// Cool background color things (it takes 3 commands to tell cpp to change a color, wtf)
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapBuffers(window);
+
+	// Event handling because cpp go brr
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 	}
 
+	// Not important, it just ends the process and all that.
+	// Kind of how I would end myself if there was an error in the tutorial i'm following, because I couldn't fix it
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
